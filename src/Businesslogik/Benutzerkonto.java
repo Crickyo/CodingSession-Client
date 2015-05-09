a833dfb179314e54a7c3a00ea054aacd900123a6
Crickyo <ndkcrick@gmail.com>
2015-05-09 20:39:12 +0200

    Richtig schoener Test, um zu gucken ob es ueber Eclipse klappt!

--- src/Businesslogik/Benutzerkonto.java
+++ src/Businesslogik/Benutzerkonto.java
@@ -1,5 +1,12 @@
 package Businesslogik;
 
-public class Benutzerkonto {
+import java.util.LinkedList;
 
+public abstract class Benutzerkonto {
+
+	// Christian Sokoli macht die schoensten Testkommentare die die Welt je gesehen hat!
+	private String emailAdresse;
+	private String passwort;
+	private static int id;
+	private LinkedList<Benutzerkonto> freunde;
 }
